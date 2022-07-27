51e676ea702f21c648898a9cd196daadca817889
Stav Kotlovsky <stavkot1999@gmail.com>
2022-07-27 16:38:01 +0300

    backend commit

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV === 'production') {
   config = require('./prod')
 } else {
   // we are in development - return the dev keys!!!
-  config = require('./dev')
+  config = require('./prod')
 }
 
 module.exports = config
