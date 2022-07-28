dbde4942fe7945a92b7ea39a38ac27280c01cf8f
Kai Nagar <kaynagar@gmail.com>
2022-07-29 01:03:05 +0300

    pulling rimon changes

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ const authRoutes = require('./api/auth/auth.routes')
 const userRoutes = require('./api/user/user.routes')
 const boardRoutes = require('./api/board/board.routes')
 const reviewRoutes = require('./api/review/review.routes')
-// const { setupSocketAPI } = require('./services/socket.service')
+const { setupSocketAPI } = require('./services/socket.service')
 
 
 // routes
@@ -54,7 +54,7 @@ app.use('/api/auth', authRoutes)
 app.use('/api/user', userRoutes)
 app.use('/api/board', boardRoutes)
 app.use('/api/review', reviewRoutes)
-// setupSocketAPI(http)
+setupSocketAPI(http)
 
 
 // Make every server-side-route to match the index.html
