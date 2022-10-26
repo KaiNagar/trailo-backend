87b6344734432c9455ee5b4ab817b9fef649ef04
Kai Nagar <kaynagar@gmail.com>
2022-10-26 13:01:33 +0300

    envKey

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'dbURL': 'mongodb+srv://kaiNagar:kaiNagar13@cluster0.6vdbt.mongodb.net/?retryWrites=true&w=majority',
+  'dbURL': `mongodb+srv://kaiNagar:${process.env.DB_KEY}@cluster0.6vdbt.mongodb.net/?retryWrites=true&w=majority`,
 }
